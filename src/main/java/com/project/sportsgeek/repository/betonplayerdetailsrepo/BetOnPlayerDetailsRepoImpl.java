9a4de25a921526ae5dc87430186da544bcfafbd3
sajeshkumar adeya <sajeshkumar1711@gmail.com>
2021-04-18 18:23:23 +0530

    18-04-2021 BetOnPlayerDetails CRUD

--- src/main/java/com/project/sportsgeek/repository/betonplayerdetailsrepo/BetOnPlayerDetailsRepoImpl.java
+++ src/main/java/com/project/sportsgeek/repository/betonplayerdetailsrepo/BetOnPlayerDetailsRepoImpl.java
@@ -33,14 +33,12 @@ public class BetOnPlayerDetailsRepoImpl implements BetOnPlayerDetailsRepo {
                 "FROM BetOnPlayerDetails as b INNER JOIN Player as p on b.PlayerId=p.PlayerId INNER JOIN Team as t on p.TeamId=t.TeamId INNER JOIN PlayerType as pt on p.TypeId=pt.PlayerTypeId INNER JOIN BetOnPlayer as bp on b.BetPlayerId=bp.BetPlayerId INNER JOIN User as u on bp.UserId=u.UserId WHERE b.BetPlayerId="+betPlayerId;
         return jdbcTemplate.query(sql, new BetPlayerDetailRowMapper());
     }
-
     @Override
     public List<BetPlayerDetailsResponse> findAllBetPlayerDetailsByUserId(int userId) throws Exception {
         String sql = "SELECT b.playerPoints as PlayerPoints, b.BetPlayerId as BetPlayerId, b.PlayerNo as PlayerNo, t.ShortName as TeamName, p.Name as Name, pt.TypeName as TypeName, p.ProfilePicture as ProfilePicture, u.Username as UserName, u.FirstName as FirstName, u.LastName as LastName " +
                 "FROM BetOnPlayerDetails as b INNER JOIN Player as p on b.PlayerId=p.PlayerId INNER JOIN Team as t on p.TeamId=t.TeamId INNER JOIN PlayerType as pt on p.TypeId=pt.PlayerTypeId INNER JOIN BetOnPlayer as bp on b.BetPlayerId=bp.BetPlayerId INNER JOIN User as u on bp.UserId=u.UserId WHERE bp.UserId="+userId;
         return jdbcTemplate.query(sql, new BetPlayerDetailRowMapper());
     }
-
     @Override
     public int addBetPlayerDetails(BetOnPlayerDetails betOnPlayerDetails) throws Exception {
         KeyHolder holder = new GeneratedKeyHolder();
@@ -48,7 +46,6 @@ public class BetOnPlayerDetailsRepoImpl implements BetOnPlayerDetailsRepo {
                 new BeanPropertySqlParameterSource(betOnPlayerDetails), holder);
         return holder.getKey().intValue();
     }
-
     @Override
     public int updateBetPlayerDetails(int betPlayerId,int playerNo, BetOnPlayerDetails betOnPlayerDetails) throws Exception {
         String sql = "UPDATE betonplayerdetails SET PlayerId="+betOnPlayerDetails.getPlayerId()+",PlayerPoints="+betOnPlayerDetails.getPlayerPoints()+" WHERE BetPlayerId="+betPlayerId+" AND PlayerNo="+playerNo;
